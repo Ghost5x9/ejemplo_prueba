2c489a07b6dd4200725561ef88d5d4a9e765bb6b
Ghost5x9 <danieltaype68@outlook.com>
2024-01-15 12:49:49 -0500

    clase lunes

---
+++ Semana 2/index.js
@@ -0,0 +1,21 @@
+function main(){
+let num = 10
+let nombre ="Edgar"
+let serHumano = true
+let altura = 1.71
+let listado = [10, 20, "20"]
+let alumno = {
+    nombre:"Pepe", 
+    altura:1.70,
+    profesor: true,
+    hobbies: ["leer","programar"],
+    universidad: {
+        nombre: "Universidad de Lima"}}
+
+
+let datoNulo = null
+let variableIndefinida = undefined
+console.log(alumno)
+}
+
+main()
